9775a41dc1c655ad104cba1c0b03157933c3c528
Qiwei Huang <hqiwei11@hotmail.com>
2016-11-22 11:47:38 -0800

    make spaceship class

--- spaceshipCode/animate.js
+++ spaceshipCode/animate.js
@@ -1,38 +1,71 @@
 $(document).ready(function(){
 
 var container = document.getElementById("container");
-var ship = document.getElementById("spaceship");
+//var ship = document.getElementById("spaceship");
 
-function moveUp(){
-	var currentVerticalPosition = ship.offsetTop;
-	if (currentVerticalPosition-8>=0)
-		ship.style.top=(currentVerticalPosition-8)+"px";
-}
-function moveDown(){
-	var currentVerticalPosition = ship.offsetTop;
-	if (currentVerticalPosition+8<=378)
-		ship.style.top=(currentVerticalPosition+8)+"px";
-}
-function moveLeft(){
-	var currentHorizontalPosition = ship.offsetLeft;
-	if (currentHorizontalPosition-8>=0)
-		ship.style.left=(currentHorizontalPosition-8)+"px";
-}
-function moveRight(){
-	var currentHorizontalPosition = ship.offsetLeft;
-	if (currentHorizontalPosition-8<=356)
-		ship.style.left=(currentHorizontalPosition+8)+"px";
-}
-function blueShipRotateDown(ship){
-	ship.style.background = 'url("split_up/spaceships_65.gif")';
-}
-function blueShipRotateUp(ship){
-	ship.style.background = 'url("split_up/spaceships_35.gif")';
-}
-function blueShipRotateBack(ship){
-	ship.style.background = 'url("split_up/spaceships_05.gif")';
-}
-
+function blueShipConstructor(){
+	var ship = document.createElement("div");
+	ship.id = "blue";
+	container.appendChild(ship);
+	ship.style.position = "absolute";
+	ship.style.width = "22px";
+	ship.style.height = "22px";
+	ship.style.background = 'url("split_up/spaceships_05.gif") no-repeat';
+	ship.style.left = "0px";
+	ship.style.top = "200px";
+	console.log(ship.offsetTop);
+	var getVertical = function(){
+		return ship.offsetTop;
+	}
+	var getHorizontal = function(){
+		return ship.offsetLeft;
+	}
+	this.moveUp = function(){
+		var currentVerticalPosition = getVertical();
+		if (currentVerticalPosition-8>=0)
+			ship.style.top=(currentVerticalPosition-8)+"px";
+	}
+	this.moveDown = function(){
+		var currentVerticalPosition = getVertical();
+		if (currentVerticalPosition-8>=0)
+			ship.style.top=(currentVerticalPosition+8)+"px";
+	}
+	this.moveLeft = function(){
+		var currentHorizontalPosition = getHorizontal();
+		if (currentHorizontalPosition-8>=0)
+			ship.style.left=(currentHorizontalPosition-8)+"px";
+	}
+	this.moveRight = function(){
+		var currentHorizontalPosition = getHorizontal();
+		if (currentHorizontalPosition-8<=356)
+			ship.style.left=(currentHorizontalPosition+8)+"px";
+	}
+	this.fire = function(){
+		var fire = document.createElement("div");
+		fire.className = "fire";
+		container.appendChild(fire);
+		fire.style.position = "absolute";
+		fire.style.width = "14px";
+		fire.style.height = "9px";
+		fire.style.background = 'url("split_up/spaceships_208L.gif") no-repeat';
+		fire.style.top = (getVertical()+8)+"px";
+		fire.style.left = (getHorizontal()+22)+"px";
+		var id = setInterval(frame, 5);
+		function frame() {
+			var pos = fire.offsetLeft;
+			if(pos === 390) {
+				clearInterval(id);
+				container.removeChild(fire);
+			}
+			else{
+				pos++;
+				fire.style.left = pos + 'px';
+			}
+		}
+		
+	}
+};
+var blueship = new blueShipConstructor();
 setInterval(function generateEnemy(){
 	var enemy = document.createElement("div");
 	enemy.className = "enemy94";
@@ -57,42 +90,19 @@ setInterval(function generateEnemy(){
 	}
 
 },8000);
-function fire(){
-	var fire = document.createElement("div");
-	fire.className = "fire";
-	container.appendChild(fire);
-	fire.style.position = "absolute";
-	fire.style.width = "14px";
-	fire.style.height = "9px";
-	fire.style.background = 'url("split_up/spaceships_208L.gif") no-repeat';
-	var shipVerticalPosition = ship.offsetTop, shipHorizontalPosition = ship.offsetLeft;
-	fire.style.top = (shipVerticalPosition+8)+"px";
-	fire.style.left = (shipHorizontalPosition+22)+"px";
-	var id = setInterval(frame, 5);
-	function frame() {
-		var pos = fire.offsetLeft;
-		if(pos === 390) {
-			clearInterval(id);
-			container.removeChild(fire);
-		}
-		else{
-			pos++;
-			fire.style.left = pos + 'px';
-		}
-	}
-}
+
 document.addEventListener("keypress",function(event){
 	console.log(event);
 	if(event.key=="i"){
-		moveUp();
+		blueship.moveUp();
 	}else if(event.key=="k"){
-		moveDown();
+		blueship.moveDown();
 	}else if(event.key=="j"){
-		moveLeft();
+		blueship.moveLeft();
 	}else if(event.key=="l"){
-		moveRight();
+		blueship.moveRight();
 	}else if(event.key=="a"){
-		fire();
+		blueship.fire();
 	}
 })
 });
